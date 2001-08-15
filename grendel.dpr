524898b7ca29f6234b590b762d7f0ed1a510fb05
Michiel Rook <mrook@php.net>
2001-08-15 19:25:21 +0000

    Changed the license model to the BSD License

--- grendel.dpr
+++ grendel.dpr
@@ -6,22 +6,33 @@
   Webpage:            http://www.grendelproject.nl/
   E-Mail:             michiel@grendelproject.nl
 
-  Please observe LICENSE.TXT prior to using this software.
+  Please observe the file "text\License.txt" before using this software.
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License, Version 2,
-  as published by the Free Software Foundation.
+  Redistribution and use in source and binary forms, with or without modification, 
+  are permitted provided that the following conditions are met:
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details (LICENSE.TXT).
+  * Redistributions of source code must retain the above copyright notice, 
+    this list of conditions and the following disclaimer. 
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  * Redistributions in binary form must reproduce the above copyright notice, 
+    this list of conditions and the following disclaimer in the documentation and/or 
+    other materials provided with the distribution. 
+  
+  Neither the name of The Grendel Project nor the names of its contributors may be used 
+  to endorse or promote products derived from this software without specific prior 
+  written permission. 
 
-  $Id: grendel.dpr,v 1.55 2001/08/14 14:17:57 ***REMOVED*** Exp $
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+  SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+  $Id: grendel.dpr,v 1.56 2001/08/15 19:25:21 ***REMOVED*** Exp $
 }
 
 program grendel;
@@ -483,7 +494,6 @@ begin
 
   writeDirect(version_info + ', ' + version_number + '.');
   writeDirect(version_copyright + '.');
-  writeDirect('This is free software, with ABSOLUTELY NO WARRANTY; view LICENSE.TXT.'#13#10);
   writeConsole('Booting server...');
 
   try
