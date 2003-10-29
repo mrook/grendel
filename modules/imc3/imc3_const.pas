7cee5e3eb81ecce22d232857c8d2a39394b465d8
Michiel Rook <mrook@php.net>
2003-10-29 12:58:09 +0000

    Added a bunch of flags

--- modules/imc3/imc3_const.pas
+++ modules/imc3/imc3_const.pas
@@ -3,7 +3,7 @@
 
 	Based on client code by Samson of Alsherok.
 
-	$Id: imc3_const.pas,v 1.1 2003/10/03 21:00:28 ***REMOVED*** Exp $
+	$Id: imc3_const.pas,v 1.2 2003/10/29 12:58:09 ***REMOVED*** Exp $
 }
 unit imc3_const;
 
@@ -17,6 +17,17 @@ const
 	MAX_I3HISTORY = 20;
 	MAX_IPS = 8192 * 16;
 	MAX_READ = 4096;
+
+const
+	I3_TELL 			= BV00;
+	I3_DENYTELL 	= BV01;
+	I3_BEEP				= BV02;
+	I3_DENYBEEP 	= BV03;
+	I3_INVIS			= BV04;
+	I3_PRIVACY		= BV05;
+	I3_DENYFINGER	= BV06;
+	I3_AFK				= BV07;
+	I3_COLOR			= BV08;
 
 const
 	I3_CHANLIST_FILE = ModulesDir + 'i3_chanlist.xml';
