0366a8e58a724ccbcb8fb85d5bc58db5d1af1a21
Michiel Rook <mrook@php.net>
2003-11-07 21:03:29 +0000

    Expand event handling

--- units/events.pas
+++ units/events.pas
@@ -2,7 +2,7 @@
   Summary:
     System events
   
-  ##  $Id: events.pas,v 1.2 2003/11/05 15:19:51 ***REMOVED*** Exp $
+  ##  $Id: events.pas,v 1.3 2003/11/07 21:03:29 ***REMOVED*** Exp $
 }
 
 unit events;
@@ -15,7 +15,7 @@ uses
 
 
 type
-	GEventFunc = function(eventname : string; var eventobject) : boolean;
+	GEventHandlerFunc = function(eventname : string; eventobject : TObject) : boolean;
 
 
 var
@@ -25,10 +25,10 @@ var
 procedure registerEvent(name : string);
 procedure unregisterEvent(name : string);
 	
-procedure registerEventHandler(name : string; handler : GEventFunc);
-procedure unregisterEventHandler(name : string; handler : GEventFunc);
+procedure registerEventHandler(name : string; handler_func : GEventHandlerFunc);
+procedure unregisterEventHandler(name : string; handler_func : GEventHandlerFunc);
 
-procedure raiseEvent(name : string; var eventobject);
+procedure raiseEvent(name : string; eventobject : TObject);
 
 procedure initEvents();
 procedure cleanupEvents();
@@ -39,6 +39,13 @@ implementation
 
 uses
 	SysUtils;
+
+
+type
+	GEventHandler = class
+		func : GEventHandlerFunc;
+		node : GListNode;
+	end;
 	
 
 procedure registerEvent(name : string);
@@ -57,44 +64,106 @@ begin
 	list := GDLinkedList(eventList[name]);
 	
 	if (list <> nil) then
+		begin
+		list.clean();
+		list.Free();
 		eventList.remove(name);
+		end;
 end;
 
-procedure registerEventHandler(name : string; handler : GEventFunc);
-begin
-end;
-
-procedure unregisterEventHandler(name : string; handler : GEventFunc);
-begin
-end;
-
-procedure raiseEvent(name : string; var eventobject);
+procedure registerEventHandler(name : string; handler_func : GEventHandlerFunc);
 var
+	handler : GEventHandler;
 	list : GDLinkedList;
-	handler : GEventFunc;
 begin
 	list := GDLinkedList(eventList[name]);
 	
 	if (list = nil) then
-		raise Exception.Create('Unknown event ' + name);
-		
-	handler(name, eventobject);
+		raise Exception.Create('Unknown event ' + name)
+	else
+		begin
+		handler := GEventHandler.Create();
+		handler.func := handler_func;
+		handler.node := list.insertLast(handler);
+		end;
+end;
+
+procedure unregisterEventHandler(name : string; handler_func : GEventHandlerFunc);
+var
+	handler : GEventHandler;
+	list : GDLinkedList;
+	iterator : GIterator;
+	found : boolean;
+begin
+	list := GDLinkedList(eventList[name]);
+	
+	if (list = nil) then
+		raise Exception.Create('Unknown event ' + name)
+	else
+		begin
+		iterator := list.iterator();
+		found := false;
+
+		while (iterator.hasNext()) do
+			begin
+			handler := GEventHandler(iterator.next());
+
+			if (@handler.func = @handler_func) then
+				begin
+				list.remove(handler.node);
+				handler.Free();
+				found := true;
+				break;
+				end;
+			end;
+
+		iterator.Free();
+
+		if (not found) then
+			raise Exception.Create('Event handler not found for "' + name + '"');
+		end;
+end;
+
+procedure raiseEvent(name : string; eventobject : TObject);
+var
+	list : GDLinkedList;
+	iterator : GIterator;
+	handler : GEventHandler;
+begin
+	list := GDLinkedList(eventList[name]);
+	
+	if (list = nil) then
+		raise Exception.Create('Unknown event ' + name)
+	else
+		begin
+		iterator := list.iterator();
+
+		while (iterator.hasNext()) do
+			begin
+			handler := GEventHandler(iterator.next());
+
+			handler.func(name, eventobject);
+			end;
+
+		iterator.Free();
+		end;
 end;
 
 procedure initEvents();
 begin
-  eventList := GHashTable.Create(128);
+	eventList := GHashTable.Create(128);
   
-  // default events
-  registerEvent('char-login');
-  registerEvent('mud-boot');
-  registerEvent('mud-shutdown');
-  registerEvent('room-enter');
+	// default events
+	registerEvent('char-login');
+	registerEvent('char-logout');
+	registerEvent('mud-boot');
+	registerEvent('mud-shutdown');
+	registerEvent('room-enter');
 end;
 
 procedure cleanupEvents();
 begin
-  eventList.clear();
+	eventList.clear();
 	eventList.Free();
 end;
 
