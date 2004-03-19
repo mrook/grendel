d66e72a79da23af2052a61bd175e3adb435bc5c5
Michiel Rook <mrook@php.net>
2004-03-19 15:39:22 +0000

    Use TForm.Release to prevent AV's

--- src/modules/status/status_console.pas
+++ src/modules/status/status_console.pas
@@ -7,37 +7,41 @@ implementation
 
 {$IFNDEF CONSOLEBUILD}
 uses
-  Windows,
-  StdCtrls,
-  SysUtils,
-  Graphics,
-  Forms,
-  console,
-  modules,
-  constants,
-  systray;
+	Windows,
+	StdCtrls,
+	SysUtils,
+	Graphics,
+	Forms,
+	console,
+	debug,
+	modules,
+	constants,
+	systray;
+
   
 type
-  GConsoleWindowWriter = class(GConsoleWriter)
-  public
-    procedure write(timestamp : TDateTime; const text : string); override;
-  end;
-  
-  GConsoleModule = class(TInterfacedObject, IModuleInterface)
-  	procedure registerModule();
-  	procedure unregisterModule();
-  end;
+	GConsoleWindowWriter = class(GConsoleWriter)
+	public
+		procedure write(timestamp : TDateTime; const text : string; debugLevel : integer = 0); override;
+	end;
+
+	GConsoleModule = class(TInterfacedObject, IModuleInterface)
+	public
+		procedure registerModule();
+		procedure unregisterModule();
+	end;
+
   
 var
-  consoleForm : TForm;
-  consoleMemo : TMemo;
-  consoleFont : TFont;
-  consoleDriver : GConsoleWindowWriter;
+	consoleForm : TForm;
+	consoleMemo : TMemo;
+	consoleFont : TFont;
+	consoleDriver : GConsoleWindowWriter;
   
 
 procedure showConsoleProc(id : integer);
 begin
-  consoleForm.Show();
+	consoleForm.Show();
 end;
  
 function returnModuleInterface() : IModuleInterface;
@@ -45,13 +49,16 @@ begin
 	Result := GConsoleModule.Create();
 end;
 
-procedure GConsoleWindowWriter.write(timestamp : TDateTime; const text : string);
+procedure GConsoleWindowWriter.write(timestamp : TDateTime; const text : string; debugLevel : integer = 0);
 begin
-	if (consoleMemo <> nil) then
+	if (consoleMemo = nil) then
+		exit;
+		
+	if (debugLevel = 0) then
 		begin
-  	consoleMemo.Lines.add('[' + FormatDateTime('hh:nn', Now) + '] ' + text);
-  	Application.ProcessMessages();
-  	end;
+		consoleMemo.Lines.add('[' + FormatDateTime('hh:nn', Now) + '] ' + text);
+		Application.ProcessMessages();
+		end;
 end;
 
 procedure GConsoleModule.registerModule();
@@ -60,51 +67,52 @@ var
 begin
 	Application.Title := 'Grendel ' + version_number;
 
-  consoleForm := TForm.Create(nil);
-  consoleForm.Caption := version_info + ': Server console';
-  consoleForm.Position := poScreenCenter;
-  consoleForm.BorderStyle := bsSingle;
-  consoleForm.BorderIcons := [biSystemMenu];
-  consoleForm.Width := 600;
-  consoleForm.Height := 400;
-  
-  consoleFont := TFont.Create();
-  consoleFont.Name := 'Courier';
-  consoleFont.Size := 10;
-  
-  consoleMemo := TMemo.Create(consoleForm);
-  consoleMemo.Width := consoleForm.ClientWidth;
-  consoleMemo.Height := consoleForm.ClientHeight;
-  consoleMemo.Parent := consoleForm;
-  consoleMemo.ScrollBars := ssVertical;
-  consoleMemo.ReadOnly := True;
-  consoleMemo.WordWrap := false;
-  consoleMemo.Font := consoleFont;
-  
-  consoleDriver := GConsoleWindowWriter.Create();
+	consoleForm := TForm.Create(nil);
+	consoleForm.Caption := version_info + ': Server console';
+	consoleForm.Position := poScreenCenter;
+	consoleForm.BorderStyle := bsSingle;
+	consoleForm.BorderIcons := [biSystemMenu];
+	consoleForm.Width := 600;
+	consoleForm.Height := 400;
 
-  registerMenuItem('Show console', showConsoleProc);
+	consoleFont := TFont.Create();
+	consoleFont.Name := 'Courier';
+	consoleFont.Size := 10;
 
-  console := GConsole.Create();
+	consoleMemo := TMemo.Create(consoleForm);
+	consoleMemo.Width := consoleForm.ClientWidth;
+	consoleMemo.Height := consoleForm.ClientHeight;
+	consoleMemo.Parent := consoleForm;
+	consoleMemo.ScrollBars := ssVertical;
+	consoleMemo.ReadOnly := True;
+	consoleMemo.WordWrap := false;
+	consoleMemo.Font := consoleFont;
 
-  console.fetchHistory(consoleDriver);  
-  console.attachWriter(consoleDriver);
+	consoleDriver := GConsoleWindowWriter.Create();
+
+	registerMenuItem('Show console', showConsoleProc);
+
+	console := GConsole.Create();
+	console.fetchHistory(consoleDriver);  
+	console.attachWriter(consoleDriver);	
+	console.Free();
 end;
 
 procedure GConsoleModule.unregisterModule();
 var
 	console : GConsole;
 begin
-  console := GConsole.Create();
-  console.detachWriter(consoleDriver);
+	console := GConsole.Create();
+	console.detachWriter(consoleDriver);
+	console.Free();
 
-  FreeAndNil(consoleMemo);
+	FreeAndNil(consoleMemo);
 	FreeAndNil(consoleFont);
-	FreeAndNil(consoleForm);
+	consoleForm.Release();
 
-  unregisterMenuItem('Show console');
-  
-  consoleDriver.Free();
+	unregisterMenuItem('Show console');
+
+	consoleDriver.Free();
 end;
 
 
