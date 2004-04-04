29b8a55ec8454b67b553fcade418b28e5e3e50af
Michiel Rook <mrook@php.net>
2004-04-04 22:39:03 +0000

    Reformatted copyright header

--- src/grendel.dpr
+++ src/grendel.dpr
@@ -1,6 +1,6 @@
 {
   The Grendel Project - A Windows/Linux MUD Server
-  Copyright (c) 2000-2004 by Michiel Rook (Grimlord)
+  Copyright (C) 2000-2004 by Michiel Rook
 
   Contact information:
   Webpage:            http://www.grendelproject.nl/
@@ -8,31 +8,32 @@
 
   Please observe the file "text\License.txt" before using this software.
 
-  Redistribution and use in source and binary forms, with or without modification, 
-  are permitted provided that the following conditions are met:
+  Redistribution and use in source and binary forms, with or without 
+  modification, are permitted provided that the following conditions are met:
 
   * Redistributions of source code must retain the above copyright notice, 
     this list of conditions and the following disclaimer. 
 
   * Redistributions in binary form must reproduce the above copyright notice, 
-    this list of conditions and the following disclaimer in the documentation and/or 
-    other materials provided with the distribution. 
+    this list of conditions and the following disclaimer in the documentation 
+    and/or other materials provided with the distribution. 
   
-  Neither the name of The Grendel Project nor the names of its contributors may be used 
-  to endorse or promote products derived from this software without specific prior 
-  written permission. 
+  Neither the name of The Grendel Project nor the names of its contributors 
+  may be used to endorse or promote products derived from this software 
+  without specific prior written permission. 
 
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
-  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
-  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
-  SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
-  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
-  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
-  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR 
+  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-  $Id: grendel.dpr,v 1.29 2004/04/04 22:06:27 ***REMOVED*** Exp $
+  $Id: grendel.dpr,v 1.30 2004/04/04 22:39:03 ***REMOVED*** Exp $
 }
 
 program grendel;
